05e92f24107f152de4aa20c4695422e00513a565
GHATOMIC <ghzatomic@gmail.com>
2024-06-17 11:07:19 -0300

    Alteracoes

--- autoloadghz.js
+++ autoloadghz.js
@@ -29,40 +29,80 @@ function resetVariaveisImportantes(){
 
 function setaVariaveisImportantes(){
     resetVariaveisImportantes()
-    selectionaObjESetaVariavel('idRunebook', 'Selecione o runebook para correr')
+    selectionaObjESetaVariavel('idRunebook', 'Selecione o runebook para correr',undefined)
 }
 
-function selectionaObjESetaVariavel(nameVar,msg){
+function selectionaObjESetaVariavel(nameVar,msg,type){
+    Orion.CancelTarget();
+    Orion.Wait(1000)
+    Orion.ClientLastTarget('');
+    Orion.Wait(1000)
+    Orion.ClearJournal()
+    Orion.Wait(1000)
     Orion.ClearJournal('self');
+    if (!type){
+        type =''
+    }
     var variavel = undefined
     if (nameVar){
         variavel = Shared.GetVar(nameVar)
     }
+    Orion.CancelTarget();
+    Orion.ClientLastTarget('');
     if (!variavel){
         Orion.CancelTarget();
-        Orion.ClientLastTarget();
+        Orion.ClientLastTarget('');
         Orion.CharPrint(self, 906, msg);
         while(!variavel){
-            Orion.Say('.mostraruid')
-            Orion.Wait(1000)
-            Orion.WaitJournal('UID', Orion.Now(), Orion.Now()+10000, 'sys');
-            if (Orion.InJournal('UID', 'sys')){
-            	var jorMSG = Orion.InJournal('UID', 'sys')
-	            if (jorMSG){
-	                 var id = jorMSG.Text().split(':')[1]
-	                 if (id){
-                        if (nameVar){
-                            Shared.AddVar(nameVar, '0x'+id.trim());
-                            variavel = Shared.GetVar(nameVar)
-                        }else{
-                            variavel = '0x'+id.trim()
+            if (type == 'mostrauid'){
+                Orion.Say('.mostraruid')
+                Orion.Wait(1000)
+                Orion.WaitJournal('UID', Orion.Now(), Orion.Now()+10000, 'sys');
+                if (Orion.InJournal('UID', 'sys')){
+                    var jorMSG = Orion.InJournal('UID', 'sys')
+                    if (jorMSG){
+                        var id = jorMSG.Text().split(':')[1]
+                        if (id){
+                            if (nameVar){
+                                Shared.AddVar(nameVar, '0x'+id.trim());
+                                variavel = Shared.GetVar(nameVar)
+                            }else{
+                                variavel = '0x'+id.trim()
+                            }
+                            Orion.CharPrint(self,906,'Setando id : '+variavel);
+                            return variavel
+                            break;
                         }
-            	 	 	Orion.CharPrint(self,906,'Setando id : '+variavel);
-                        return variavel
-            	 		break;
-	                 }
-	                 
-	            }
+                        
+                    }
+                }
+            }else{
+                Orion.Info();
+                Orion.WaitTargetObject('lasttarget');
+                var target = undefined
+                while(Orion.ClientLastTarget()==0){
+                    Orion.Wait(1000)
+                }
+                target = Orion.ClientLastTarget();
+                TextWindow.Close();
+                if (target != 0){
+                    var id = Orion.GetSerial(target)
+                    if (nameVar){
+                        Shared.AddVar(nameVar, id.trim());
+                        variavel = Shared.GetVar(nameVar)
+                    }else{
+                        variavel = id.trim()
+                    }
+                    Orion.CharPrint(self,906,'Setando id : '+variavel);
+                    TextWindow.Close();
+                    Orion.Wait(1000)
+                    Orion.CancelTarget();
+                    Orion.ClientLastTarget('');
+                    Orion.ClearJournal()
+                    Orion.Wait(1000)
+                    return variavel
+                }
+                
             }
         }
     }
